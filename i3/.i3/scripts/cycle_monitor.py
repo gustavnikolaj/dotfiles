db935c020c9fa4b50aace3248d5b5d58a6645675
Gustav Nikolaj Olsen <gno@one.com>
2013-09-08 22:37:59 +0200

    Move workspace to next output

--- i3/.i3/scripts/cycle_monitor.py
+++ i3/.i3/scripts/cycle_monitor.py
@@ -1,18 +1,41 @@
 import json
 import subprocess
 
+
 I3MSG = '/usr/bin/i3-msg'
 
-result = subprocess.Popen([I3MSG, '-t', 'get_workspaces'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-result = result.communicate()[0]
-data = json.loads(result)
 
-focused = filter(lambda x: x['focused'] == True, data)[0]
-maybeNext = filter(lambda x: x['visible'] == True, data)
+def parse():
+    mode = False
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "move":
+            mode = sys.argv[1]
+        else:
+            sys.exit("invalid parameter given")
+    return mode
 
-if maybeNext.index(focused) + 1 == len(maybeNext):
-    nextWorkspace = 0
-else:
-    nextWorkspace = maybeNext.index(focused) + 1
 
-subprocess.call([I3MSG, 'workspace', str(maybeNext[nextWorkspace]['num'])])
+def main():
+    result = subprocess.Popen([I3MSG, '-t', 'get_workspaces'],
+                              stdout=subprocess.PIPE,
+                              stdin=subprocess.PIPE)
+    result = result.communicate()[0]
+    data = json.loads(result)
+
+    focused = filter(lambda x: x['focused'] == True, data)[0]
+    maybeNext = filter(lambda x: x['visible'] == True, data)
+
+    if maybeNext.index(focused) + 1 == len(maybeNext):
+        nextWorkspace = 0
+    else:
+        nextWorkspace = maybeNext.index(focused) + 1
+
+    if mode == "move":
+        output = maybeNext[nextWorkspace]['output']
+        subprocess.call([I3MSG, 'move workspace to output', output])
+    else:
+        subprocess.call([I3MSG, 'workspace', str(maybeNext[nextWorkspace]['num'])])
+
+
+if __name__ == '__main__':
+    main()
