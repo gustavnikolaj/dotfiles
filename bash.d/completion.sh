749946f2458e59feb713c4405b12c71e040ac50d
Gustav Nikolaj Olsen <gno@one.com>
2014-10-09 11:01:47 +0200

    add nvm completion

--- bash.d/completion.sh
+++ bash.d/completion.sh
@@ -1,2 +1,5 @@
 #  possible, add tab completion for many more commands
 [ -f /etc/bash_completion ] && source /etc/bash_completion
+
+# nvm tab completion
+[[ -r $NVM_DIR/bash_completion ]] && . $NVM_DIR/bash_completion
