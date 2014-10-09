eafcce77328e9f47fc54fdc14735d56b9bbdcd4e
Gustav Nikolaj Olsen <gno@one.com>
2014-10-09 11:02:34 +0200

    add quotes around paths

--- bash.d/path.sh
+++ bash.d/path.sh
@@ -1,8 +1,8 @@
-export PATH=~/.bin:$PATH
-export PATH=~/.local/bin:$PATH
-export PATH=./node_modules/.bin:$PATH
-export PATH=/usr/local/share/npm/bin:$PATH
-export PATH=/usr/local/bin:$PATH
+export PATH="~/.bin:$PATH"
+export PATH="~/.local/bin:$PATH"
+export PATH="./node_modules/.bin:$PATH"
+export PATH="/usr/local/share/npm/bin:$PATH"
+export PATH="/usr/local/bin:$PATH"
 
 export PATH=${PATH}:~/.local/lib/androidsdk/adt-bundle-linux-x86_64-20131030/sdk/platform-tools
 export PATH=${PATH}:~/.local/lib/androidsdk/adt-bundle-linux-x86_64-20131030/sdk/tools
