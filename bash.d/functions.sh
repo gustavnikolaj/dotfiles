a664b2f5b6805b0fde0e1e4dfab03164fb0a135b
Gustav Nikolaj Olsen <gno@one.com>
2014-10-09 17:17:19 +0200

    p: sort by length of match, and add comments

--- bash.d/functions.sh
+++ bash.d/functions.sh
@@ -14,13 +14,22 @@ function onegit() {
 
 function p() {
     if [ -z $1 ]; then
+        # If none arguments or the first argument was empty, go to the projects dir
         cd ~/Projects
     else
+        # Find all folders in the folder Projects.
         local projects="`find ~/Projects/ -mindepth 1 -maxdepth 1 -type d -exec basename {} \;`"
+
+        # For each argument, grep for folders that match that argument in the remaining folders
         for arg in $@
         do
             local projects="`echo "$projects" | grep "$arg"`"
         done
+
+        # Sort the folders after lenght of the name.
+        local projects="`echo "$projects" | awk '{print length, $0}' | sort -n | cut -d " " -f2-`"
+
+        # Go to the first folder in the filtered list of folders
         cd ~/Projects/`echo "$projects" | head -n 1`
     fi
 }
