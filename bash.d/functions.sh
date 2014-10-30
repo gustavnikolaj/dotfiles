63faf3d9f1888e0eeb72f388d92ddb02c0378334
Gustav Nikolaj Olsen <gno@one.com>
2014-10-30 13:30:11 +0100

    modify p to try guessing with z if z is available

--- bash.d/functions.sh
+++ bash.d/functions.sh
@@ -30,7 +30,18 @@ function p() {
         local projects="`echo "$projects" | awk '{print length, $0}' | sort -n | cut -d " " -f2-`"
 
         # Go to the first folder in the filtered list of folders
-        cd ~/Projects/`echo "$projects" | head -n 1`
+        local destination=`echo "$projects" | head -n 1`
+        if [ -z $destination ]; then
+            # If we dont have a match use z to try to make a guess of what you meant
+            # and if the z command is not defined, just go to the projects dir.
+            if [ -z "`type -t z`" ]; then
+                cd ~/Projects
+            else
+                z $@
+            fi
+        else
+            cd ~/Projects/$destination
+        fi
     fi
 }
 
