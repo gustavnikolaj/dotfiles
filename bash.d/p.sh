e7ce3fa2576a44f685b68931c994545a509c0ce5
Gustav Nikolaj Olsen <gno@one.com>
2014-10-30 13:36:50 +0100

    make projects dir in p method configurable

--- bash.d/p.sh
+++ bash.d/p.sh
@@ -1,10 +1,12 @@
+export PROJECTSS_DIR=~/Projects
+
 function p() {
     if [ -z $1 ]; then
         # If none arguments or the first argument was empty, go to the projects dir
-        cd ~/Projects
+        cd $PROJECTS_DIR
     else
-        # Find all folders in the folder Projects.
-        local projects="`find ~/Projects/ -mindepth 1 -maxdepth 1 -type d -exec basename {} \;`"
+        # Find all folders in PROJECTS_DIR
+        local projects="`find $PROJECTS_DIR -mindepth 1 -maxdepth 1 -type d -exec basename {} \;`"
 
         # For each argument, grep for folders that match that argument in the remaining folders
         for arg in $@
@@ -21,19 +23,19 @@ function p() {
             # If we dont have a match use z to try to make a guess of what you meant
             # and if the z command is not defined, just go to the projects dir.
             if [ -z "`type -t z`" ]; then
-                cd ~/Projects
+                cd $PROJECTS_DIR
             else
                 z $@
             fi
         else
-            cd ~/Projects/$destination
+            cd $PROJECTS_DIR/$destination
         fi
     fi
 }
 
 function _projects_complete_() {
     local word=${COMP_WORDS[COMP_CWORD]}
-    local projectdirs=$(find ~/Projects/ -mindepth 1 -maxdepth 1 -type d -exec basename {} \; | tr '\n' ' ')
+    local projectdirs=$(find $PROJECTS_DIR -mindepth 1 -maxdepth 1 -type d -exec basename {} \; | tr '\n' ' ')
     COMPREPLY=($(compgen -W "$projectdirs" -- "${word}"))
 }
 complete -F _projects_complete_ p
