3d99a42b7568e71dd5a6e4918323524e5823214b
Gustav Nikolaj Olsen <gno@one.com>
2015-05-12 10:35:41 +0200

    red # when root, green $ when not

--- bash.d/prompt.sh
+++ bash.d/prompt.sh
@@ -15,6 +15,17 @@ function parse_dir_name() {
     fi
 }
 
+# print a green $ if not root and a red # if root
+# useful when doing sudo -s
+function shell_indicator() {
+    if [ "`whoami`" == "root" ]
+    then
+        echo " \[\033[31m\033[1m\]#\[\033[22m\033[39m\] "
+    else
+        echo " \[\033[32m\033[1m\]$\[\033[22m\033[39m\] "
+    fi
+}
+
 bold=`echo -e "\033[1m"`
 normal=`echo -e "\033[0m"`
 green=`echo -e "\033[32m"`
@@ -22,4 +33,4 @@ green=`echo -e "\033[32m"`
 export PS1=" \[$bold\]\$(parse_dir_name)\$([[ -n \$(git branch 2> /dev/null) ]] && echo '\[$normal\] on \[$bold\]')\$(parse_git_branch) \[$green\]\$\[$normal\] "
 export PS2="→ "
 
-export PS1=" \[\033[1m\033[90m\033[38m\]\$(parse_dir_name)\[\033[39m\033[22m\]\$([[ -n \$(git branch 2> /dev/null) ]] && echo ' on '\[\033[1m\033[90m\033[38m\]\$(parse_git_branch))\[\033[39m\033[22m\] \[\033[32m\033[1m\]$\[\033[22m\033[39m\] "
+export PS1=" \[\033[1m\033[90m\033[38m\]\$(parse_dir_name)\[\033[39m\033[22m\]\$([[ -n \$(git branch 2> /dev/null) ]] && echo ' on '\[\033[1m\033[90m\033[38m\]\$(parse_git_branch))\[\033[39m\033[22m\] $(shell_indicator)"
