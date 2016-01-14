38430828120d146529c27e75ae14b95cd2437627
Gustav Nikolaj Olsen <gno@one.com>
2016-01-14 18:50:43 +0100

    change screenlayout

--- screenlayout/laptop.sh
+++ screenlayout/laptop.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 killall -SIGSTOP i3
 
-xrandr --output eDP1 --auto --primary
-xrandr --output DP2-2 --off
-xrandr --output DP2-1 --off
+sleep 1
+
+xrandr -d :0.0 --output eDP1 --auto --primary
+xrandr -d :0.0 --output DP2-2 --off
+xrandr -d :0.0 --output DP2-1 --off
 
 killall -SIGCONT i3
--- screenlayout/work.sh
+++ screenlayout/work.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 killall -SIGSTOP i3
 
-xrandr --output eDP1 --auto --primary
+sleep 1
+
+xrandr -d :0.0 --output eDP1 --auto --primary
 # enable external monitors
-xrandr --output DP2-1 --mode 1920x1200 --right-of eDP1 --primary
-xrandr --output DP2-2 --mode 1920x1200 --right-of DP2-1
+xrandr -d :0.0 --output DP2-1 --mode 1920x1200 --right-of eDP1 --primary
+xrandr -d :0.0 --output DP2-2 --mode 1920x1200 --right-of DP2-1
 # disable internal screen
-xrandr --output eDP1 --off
+xrandr -d :0.0 --output eDP1 --off
 
 killall -SIGCONT i3
