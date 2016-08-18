0b5dd2bca089a41837525b7cf9bfd96c15eefc1e
Gustav Nikolaj Olsen <gno@one.com>
2016-08-18 13:38:52 +0200

    apps/chrome: use apt repo instead

--- apps/chrome.install.sh
+++ apps/chrome.install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 function main {
-  if [ 0 == `dpkg -s google-chrome-stable &> /dev/null ; echo $?` ] ; then
-    echo "Skipping: google-chrome (already installed)"
-  else
-    tmpdir="/tmp/chrome-install-$(date +"%s")"
-    mkdir -p $tmpdir
-    curl -L -o $tmpdir/chrome.deb \
-    https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo dpkg -i $tmpdir/chrome.deb
-    rm -rf $tmpdir
-    echo Done: google-chrome
-  fi
+    if [ 0 == `dpkg -s google-chrome-stable &> /dev/null ; echo $?` ] ; then
+      echo "Skipping: google-chrome (already installed)"
+    else
+      echo "Installing: google-chrome"
+      echo "deb [arch=amd64] https://dl-ssl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+      wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+      sudo apt-get update
+      sudo apt-get install google-chrome-stable -y
+      echo Done: google-chrome
+    fi
 }
 
 main
