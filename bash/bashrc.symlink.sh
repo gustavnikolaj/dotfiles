fd73ffa019dbcfbcfcef15e2b44eef17fa4219be
Gustav Nikolaj Olsen <gno@one.com>
2017-07-25 13:32:39 +0200

    use hardcoded paths instead of readlink -f

--- bash/bashrc.symlink.sh
+++ bash/bashrc.symlink.sh
@@ -10,7 +10,12 @@ function sourceFiles() {
   done
 }
 
-DOTFILES_DIR=$(cd $(dirname $(readlink -f ${BASH_SOURCE[0]}))/.. && pwd)
+#DOTFILES_DIR=$(cd $(dirname $(readlink -f ${BASH_SOURCE[0]}))/.. && pwd)
+DOTFILES_DIR=~/dotfiles
 
 sourceFiles $(find $DOTFILES_DIR/bash/bash.d -type f)
 sourceFiles $(find $DOTFILES_DIR -type f -name "*.bashrc.sh")
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- bin/bin.bashrc.sh
+++ bin/bin.bashrc.sh
@@ -1,2 +1,3 @@
-BIN_DIRNAME=$(cd $(dirname $(readlink -f ${BASH_SOURCE[0]})) && pwd)
+#BIN_DIRNAME=$(cd $(dirname $(readlink -f ${BASH_SOURCE[0]})) && pwd)
+BIN_DIRNAME=~/dotfiles/bin
 export PATH="$BIN_DIRNAME:$PATH"
