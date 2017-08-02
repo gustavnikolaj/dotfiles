6c3bd2cf5f86b5969ee04d445a665eece0dd20a5
Gustav Nikolaj Olsen <gno@one.com>
2017-08-02 10:47:30 +0200

    remove extraneous nvm setup added by accident

--- bash/bashrc.symlink.sh
+++ bash/bashrc.symlink.sh
@@ -16,6 +16,3 @@ DOTFILES_DIR=~/dotfiles
 sourceFiles $(find $DOTFILES_DIR/bash/bash.d -type f)
 sourceFiles $(find $DOTFILES_DIR -type f -name "*.bashrc.sh")
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
