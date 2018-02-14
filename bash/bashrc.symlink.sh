d332e99f7a909ec5c23bf5edb7470237e812ecdb
Gustav Nikolaj Olsen <gno@one.com>
2018-02-14 19:25:39 +0100

    Make sure to run local files last

--- bash/bashrc.symlink.sh
+++ bash/bashrc.symlink.sh
@@ -13,6 +13,8 @@ function sourceFiles() {
 #DOTFILES_DIR=$(cd $(dirname $(readlink -f ${BASH_SOURCE[0]}))/.. && pwd)
 DOTFILES_DIR=~/dotfiles
 
-sourceFiles $(find $DOTFILES_DIR/bash/bash.d -type f)
+sourceFiles $(find $DOTFILES_DIR/bash/bash.d -type f | grep -v bash.d/local.sh)
 sourceFiles $(find $DOTFILES_DIR -type f -name "*.bashrc.sh")
 
+# Make sure to source the local stuff last
+sourceFiles $DOTFILES_DIR/bash/bash.d/local.sh
