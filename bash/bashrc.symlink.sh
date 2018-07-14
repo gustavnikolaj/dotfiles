70855337204297e82cbcee1b8e6ddca1e4caf26a
Gustav Nikolaj Olsen <gustavnikolaj@gmail.com>
2018-07-15 01:15:22 +0200

    Prepare inlining

--- bash/bashrc.symlink.sh
+++ bash/bashrc.symlink.sh
@@ -13,7 +13,10 @@ function sourceFiles() {
 #DOTFILES_DIR=$(cd $(dirname $(readlink -f ${BASH_SOURCE[0]}))/.. && pwd)
 DOTFILES_DIR=~/dotfiles
 
+# XXX: Remove the following line
 sourceFiles $(find $DOTFILES_DIR/bash/bash.d -type f | grep -v bash.d/local.sh)
+
+# Load the *.bashrc.sh files from the topic folders in the DOTFILES_DIR
 sourceFiles $(find $DOTFILES_DIR -type f -name "*.bashrc.sh")
 
 # Make sure to source the local stuff last
