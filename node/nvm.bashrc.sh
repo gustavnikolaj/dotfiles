bf7bf5b58df5760aa1477ee6321308be8063f5f9
Gustav Nikolaj Olsen <gustavnikolaj@gmail.com>
2018-09-24 12:40:42 +0200

    Only load nvm if NVM_DIR is not set

--- node/nvm.bashrc.sh
+++ node/nvm.bashrc.sh
@@ -1,6 +1,8 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-[[ -r $NVM_DIR/bash_completion ]] && . $NVM_DIR/bash_completion
+if [ -z ${NVM_DIR+x} ]; then
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    [[ -r $NVM_DIR/bash_completion ]] && . $NVM_DIR/bash_completion
+fi
 
 source $DOTFILES_DIR/modules/p-load-nvmrc/p-load-nvmrc.sh
 
