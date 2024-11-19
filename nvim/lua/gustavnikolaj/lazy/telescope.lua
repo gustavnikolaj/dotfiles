1b933007a030243db8f283b1b19614097b0bde2a
Gustav Nikolaj <gustavnikolaj@gmail.com>
2024-11-19 01:20:20 +0100

    include hidden files in telescope

--- nvim/lua/gustavnikolaj/lazy/telescope.lua
+++ nvim/lua/gustavnikolaj/lazy/telescope.lua
@@ -11,7 +11,13 @@ return {
 		}
 
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        --vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pf', function()
+            builtin.find_files({
+                hidden = true,
+                file_ignore_patterns = { "%.git/" }
+            })
+        end, {})
 		vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 		vim.keymap.set('n', '<C-b>', builtin.buffers, {})
 		-- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
