51c80027b717792f80aad5c0c0b64638157cda23
Gustav Nikolaj <gustavnikolaj@gmail.com>
2025-07-30 01:27:46 +0200

    setup 1password

--- ubuntu-sway/install.sh
+++ ubuntu-sway/install.sh
@@ -22,3 +22,13 @@ if ! command -v brave-browser ; then
     sudo apt update
     sudo apt install brave-browser
 fi
+
+if ! command -v 1password ; then
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+    echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+    sudo apt update && sudo apt install 1password
+fi
