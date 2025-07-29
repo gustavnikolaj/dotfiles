dd6be968a1a2f24e58ac617ba08dc85addc9868b
Gustav Nikolaj <gustavnikolaj@gmail.com>
2025-07-30 01:16:49 +0200

    fix nvidia install setup steps

--- ubuntu-sway/install.sh
+++ ubuntu-sway/install.sh
@@ -4,11 +4,12 @@
 sudo apt install sway xwayland sway-backgrounds waybar wofi wl-clipboard
 
 # if nvidia drivers are in use
-if command -v nvidia-smi ; then
+if ! command -v nvidia-smi ; then
 	# Add the --unsupported-gpu flag to /usr/share/wayland-sessions/sway.desktop 
 	# Disable the hardware cursor when using proprietary nvidia drivers
 	# vim .config/environment.d/nvidia-cursor.conf
 	# WLR_NO_HARDWARE_CURSORS=1
+	echo "need to do nvidia setup"
 fi
 
 
