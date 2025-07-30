c714e8dda02901b6c38640060893d1c23f092620
Gustav Nikolaj <gustavnikolaj@gmail.com>
2025-07-30 02:44:55 +0200

    switch to waybar

--- ubuntu-sway/install.sh
+++ ubuntu-sway/install.sh
@@ -40,3 +40,34 @@ if ! command -v uwsm ; then
   echo "Go build it! https://github.com/Vladimir-csp/uwsm?tab=readme-ov-file#1-building-and-installing" 
   false
 fi
+
+# fonts
+sudo apt install -y fonts-font-awesome fonts-noto fonts-noto-color-emoji fonts-noto-cjk
+mkdir -p ~/.local/share/fonts
+
+if ! fc-list | grep -qi "CaskaydiaMono Nerd Font"; then
+  cd /tmp
+  wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
+  unzip CascadiaMono.zip -d CascadiaFont
+  cp CascadiaFont/CaskaydiaMonoNerdFont-Regular.ttf ~/.local/share/fonts
+  cp CascadiaFont/CaskaydiaMonoNerdFont-Bold.ttf ~/.local/share/fonts
+  cp CascadiaFont/CaskaydiaMonoNerdFont-Italic.ttf ~/.local/share/fonts
+  cp CascadiaFont/CaskaydiaMonoNerdFont-BoldItalic.ttf ~/.local/share/fonts
+  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-Regular.ttf ~/.local/share/fonts
+  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-Bold.ttf ~/.local/share/fonts
+  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-Italic.ttf ~/.local/share/fonts
+  cp CascadiaFont/CaskaydiaMonoNerdFontPropo-BoldItalic.ttf ~/.local/share/fonts
+  rm -rf CascadiaMono.zip CascadiaFont
+  fc-cache
+  cd -
+fi
+
+if ! fc-list | grep -qi "iA Writer Mono S"; then
+  cd /tmp
+  wget -O iafonts.zip https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.zip
+  unzip iafonts.zip -d iaFonts
+  cp iaFonts/iA-Fonts-master/iA\ Writer\ Mono/Static/iAWriterMonoS-*.ttf ~/.local/share/fonts
+  rm -rf iafonts.zip iaFonts
+  fc-cache
+  cd -
+fi        
