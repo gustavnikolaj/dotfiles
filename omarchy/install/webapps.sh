c2cd15aabf529b418fb6d6b93e1f471ad46375af
Gustav Nikolaj Olsen <gustav.olsen@group.one>
2025-10-24 11:34:06 +0200

    Cleanup webapps

---
+++ omarchy/install/webapps.sh
@@ -0,0 +1,21 @@
+export PATH=$OMARCHY_PATH/bin:$PATH
+
+omarchy-webapp-install "Outlook Calendar" https://outlook.office.com/calendar/view/workweek Calendar.png
+omarchy-webapp-install "Word" https://word.cloud.microsoft/ Word.png
+omarchy-webapp-install "Excel" https://excel.cloud.microsoft/ Excel.png
+omarchy-webapp-install "Powerpoint" https://powerpoint.cloud.microsoft/ Powerpoint.png
+omarchy-webapp-install "Teams" https://teams.microsoft.com/ Teams.png
+omarchy-webapp-install "Copilot Chat" https://m365.cloud.microsoft/chat/ Copilot.png
+
+omarchy-webapp-remove "HEY"
+omarchy-webapp-remove "Basecamp"
+omarchy-webapp-remove "WhatsApp"
+omarchy-webapp-remove "Google Photos"
+omarchy-webapp-remove "Google Contacts"
+omarchy-webapp-remove "Google Messages"
+omarchy-webapp-remove Youtube
+omarchy-webapp-remove GitHub
+omarchy-webapp-remove X
+omarchy-webapp-remove Figma
+omarchy-webapp-remove Discord
+omarchy-webapp-remove Zoom
