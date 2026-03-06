1a3c23314171090c78129d29693bdbb12a7f12b9
Gustav Nikolaj Olsen <gustav.olsen@group.one>
2026-03-06 16:18:31 +0100

    Reset hyprland.conf to the defaults from latest version of omarchy

--- omarchy/update-config-from-omarchy-defaults.sh
+++ omarchy/update-config-from-omarchy-defaults.sh
@@ -1,2 +1,3 @@
+cp ~/.local/share/omarchy/config/hypr/hyprland.conf omarchy/config/hypr/hyprland.conf
 cp ~/.local/share/omarchy/config/hypr/input.conf ~/dotfiles/omarchy/config/hypr/input.conf
 cp ~/.local/share/omarchy/config/hypr/bindings.conf omarchy/config/hypr/bindings.conf
